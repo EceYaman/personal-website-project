917e2d224a99b9d69184c2e4cf9b4a67c4e5ac97
Ece Yaman <eceymn35@gmail.com>
2025-02-03 17:02:08 +0300

    Fetching data from reqres API using axios

--- personal-website/src/contexts/DataContext.jsx
+++ personal-website/src/contexts/DataContext.jsx
@@ -1,21 +1,40 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { data } from "../data";
 import { useLocalStorage } from "../hooks/useLocalStorage";
+import axios from "axios";
 
 export const DataContext = createContext();
 
 export function DataContextProvider({ children }) {
   const [language, setLanguage] = useLocalStorage("language", "en");
   const [currData, setCurrData] = useState(data.en); 
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setCurrData(data[language]);
-  }, [language]);
+    const sendData = async () => {
+      try {
+        const response = await axios.post('https://reqres.in/api/workintech', data[language]);
+        setCurrData(response.data);  
+        setLoading(false); 
+        console.log(response.data);  
+      } catch (error) {
+        console.error('Error:', error);
+        setLoading(false);  
+      }
+    };
+    sendData();  
+  }, [language]); 
 
   const toggleLanguage = () => {
     const newLanguage = language === "en" ? "tr" : "en";
     setLanguage(newLanguage); 
   };
+
+
+  if (loading) {
+    return <div>Loading...</div>;
+}
+
 
   return (
     <DataContext.Provider value={{ currData, toggleLanguage }}>
--- personal-website/src/main.jsx
+++ personal-website/src/main.jsx
@@ -3,12 +3,15 @@ import './index.css'
 import App from './App.jsx'
 import { DataContextProvider } from './contexts/DataContext.jsx'
 import { DarkModeProvider } from './contexts/ThemeContext.jsx'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
     <DarkModeProvider>
         <DataContextProvider>
             <App />
         </DataContextProvider>
+        <ToastContainer />
     </DarkModeProvider>
   
 )
